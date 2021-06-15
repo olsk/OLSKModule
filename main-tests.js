42e70078808233a0c44f663897be7f744e5454b4
Rosano Coutinho <pub@xeari.com>
2021-06-15 11:52:01 -0300

    Spec OLSKModuleSetupKeys

--- main-tests.js
+++ main-tests.js
@@ -2,5 +2,23 @@ const { deepEqual, throws } = require('assert');
 
 const mod = require('./main.js');
 
+describe('OLSKModuleSetupKeys', function test_OLSKModuleSetupKeys() {
+
+	it('throws if not object', function () {
+		throws(function () {
+			mod.OLSKModuleSetupKeys(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns array', function () {
+		const signature = 'Setup' + uRandomInt();
+		const item = {
+			['_' + signature]: Math.random().toString(),
+			[signature]: Math.random().toString(),
+		};
+
+		deepEqual(mod.OLSKModuleSetupKeys(item), [signature]);
+	});
+
 });
 
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const mod = {
+
+	OLSKModuleSetupKeys (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return Object.keys(inputData).filter(function (e) {
+			return e.match(/^Setup/);
+		});
+	},
+	
 };
 
 Object.assign(exports, mod);
